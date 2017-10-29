5a2d915d9d5491062dfc988a8b2961e4dbbf3fe2
ACERY1 <y841034081@gmail.com>
2017-10-29 15:25:23 +0800

    add sass syntax

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 简介：
 
-一个基于MVC的项目，技术栈是Express+Mongodb+Pug+Typescript+BootStrap
+一个基于MVC的项目，技术栈是Express+Mongodb+Pug+Typescript+BootStrap+SCSS
--- src/server.ts
+++ src/server.ts
@@ -28,8 +28,6 @@ const port = process.env.PORT || 3000;
  */
 mongoose.connect(process.env.mongodbURI);
 
-
-
 mongoose.connection.on('error', () => {
     console.log('mongodb connection error!')
 });
@@ -40,14 +38,18 @@ mongoose.connection.on('open', () => {
 
 
 /**
- * express router config
+ * express global configuration
  */
-app.set("views", path.join(__dirname, "../views")); // 绑定MVC中的View层
-app.set("view engine", "pug"); // 使用渲染引擎
-app.use(logger("dev"));
+app.set("views", path.join(__dirname, "../views"));  // 绑定MVC中的View层
+app.set("view engine", "pug");  // 使用渲染引擎
+app.use(logger("dev"));  // 使用express 自带 logger -Morgan /*dev common combined short tiny*/
+app.use(bodyParser.json());  // 处理http请求body里的application/json数据
+app.use(bodyParser.urlencoded({ extended: false }));  // for application/x-www-form-urlencoded
+app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })); // 使用express静态转发，/js将转发到/public/js
 
-
-
+/**
+ * express router configuration
+ */
 app.get('/',(req,res)=>{
     res.render('home')
 });
